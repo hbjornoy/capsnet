894735f04834a5274acc6e77bc0631d5707d5f2d
hbjornoy <havard.bjornoy@gmail.com>
2019-11-18 14:57:51 +0100

    remove data omniglotdata and write script to download

---
+++ download_omniglot.sh
@@ -0,0 +1,18 @@
+if [ ! -f "images_background.zip" ]; then
+    wget http://github.com/brendenlake/omniglot/raw/master/python/images_background.zip
+fi
+if [ ! -f "images_evaluation.zip" ]; then
+    wget http://github.com/brendenlake/omniglot/raw/master/python/images_evaluation.zip
+fi
+
+mkdir -p language_dataset
+
+if [ ! -d "language_dataset/images_background" ]; then
+    unzip images_background.zip -d language_dataset
+fi
+if [ ! -d "language_dataset/images_evaluation" ]; then
+    unzip images_evaluation.zip -d language_dataset
+fi
+
+rm -rf images_background.zip
+rm -rf images_evaluation.zip
